b3772a15539bf8fe313bc5c6bd19432d81ee2e1b
Gheorghe Ileana Daniela <ileanadaniela19@yahoo.com>
2018-06-13 13:19:51 +0300

    modificari2

---
+++ app/src/main/java/ro/ase/eu/aslapplication/activitatiCategorii/AnimaleActivity.java
@@ -0,0 +1,153 @@
+package ro.ase.eu.aslapplication.activitatiCategorii;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import ro.ase.eu.aslapplication.BaseActivity;
+import ro.ase.eu.aslapplication.R;
+
+public class AnimaleActivity extends BaseActivity {
+
+    private static final String IMAGES_URL="http://ileanadaniela19.000webhostapp.com/Animale/getAnimale.php";
+    Button btnNext,btnPrev;
+    WebView webView;
+    TextView textView;
+    private String imagesJSON;
+
+    private static final String JSON_ARRAY ="result";
+    private static final String IMAGE_URL = "url";
+    private static final String IMAGE_NAME="nume";
+
+    private JSONArray arrayImages= null;
+    private int TRACK = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animale);
+
+        Intent intent=getIntent();
+
+        btnNext=(Button) findViewById(R.id.buttonNext);
+        btnPrev=(Button)findViewById(R.id.buttonPrev);
+        webView=(WebView) findViewById(R.id.wvAnimaleA);
+        textView=(TextView)findViewById(R.id.tvAnimaleA);
+
+        webView.setBackgroundColor(Color.WHITE);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getAllImages();
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                movePrevious();
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveNext();
+            }
+        });
+    }
+
+    private void extractJSON(){
+        try {
+            JSONObject jsonObject = new JSONObject(imagesJSON);
+            arrayImages = jsonObject.getJSONArray(JSON_ARRAY);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showImage(){
+        try {
+            JSONObject jsonObject = arrayImages.getJSONObject(TRACK);
+            webView.loadUrl(jsonObject.getString(IMAGE_URL));
+            textView.setText(jsonObject.getString(IMAGE_NAME).toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void moveNext(){
+        if(TRACK < arrayImages.length()){
+            TRACK++;
+            showImage();
+        }
+    }
+
+    private void movePrevious(){
+        if(TRACK>0){
+            TRACK--;
+            showImage();
+        }
+    }
+
+
+
+    private void getAllImages() {
+        class GetAllImages extends AsyncTask<String,Void,String> {
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(AnimaleActivity.this, "Încărcare imagini...","Vă rugăm asteptați...",true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                imagesJSON = s;
+                extractJSON();
+                showImage();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String uri = params[0];
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while((json = bufferedReader.readLine())!= null){
+                        sb.append(json+"\n");
+                    }
+
+                    return sb.toString().trim();
+
+                }catch(Exception e){
+                    return null;
+                }
+            }
+        }
+        GetAllImages gai = new GetAllImages();
+        gai.execute(IMAGES_URL);
+    }
+}
